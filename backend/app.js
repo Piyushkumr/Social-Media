cb4cf61a24143f3d60413ab296f6f4e8647a9405
piyush <piyush7206@gmail.com>
2025-09-15 02:34:56 +0530

    Frontend

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
+const cors = require("cors");
 
 if(process.env.NODE_ENV !== "production"){
     require("dotenv").config({path: "backend/config/config.env"});
 }
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
 
 // Using middleware
 app.use(express.json({ limit: "50mb"}));
